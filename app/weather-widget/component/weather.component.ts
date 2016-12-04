66a878e0752f334b2e0cd6fc56879afebda120dd
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-04 08:30:11 +0300

    initial commit

---
+++ app/weather-widget/component/weather.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { WeatherService } from '../service/weather.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'weather-widget',
+    templateUrl: 'weather.component.html',
+    styleUrls: ['weather.component.css'],
+    providers: [ WeatherService ]
+})
+
+export class WeatherComponent{
+    pos: Position;
+
+    constructor(private service: WeatherService){
+        this.service.getCurrentLocation()
+            .subscribe(position => {
+                this.pos = position;
+                this.service.getCurrentWeather(this.pos.coords.latitude, this.pos.coords.longitude)
+                .subscribe(weather => console.log(weather),
+                err => console.error(err));
+            },
+            err => console.error(err));
+        
+    }
+}
---
+++ app/weather-widget/constants/constants.example.ts
@@ -0,0 +1,2 @@
+export const FORECAST_KEY = "your key";
+export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
