375296312a5f9a65d68b155858f17166f60e07d1
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-05 09:19:36 +0300

    weather icons

--- app/weather-widget/component/weather.component.ts
+++ app/weather-widget/component/weather.component.ts
@@ -4,6 +4,8 @@ import { Weather } from '../model/weather';
 
 import { WeatherService } from '../service/weather.service';
 
+declare var Skycons: any;
+
 @Component({
     moduleId: module.id,
     selector: 'weather-widget',
@@ -18,6 +20,7 @@ export class WeatherComponent implements OnInit {
     currentSpeedUnit = "kph";
     currentTempUnit = "celsius";
     currentLocation = '';
+    icons = new Skycons({"color": "#FFF"});
 
     constructor(private service: WeatherService){ }
 
@@ -44,6 +47,7 @@ export class WeatherComponent implements OnInit {
                 this.weatherData.humidity = weather["currently"]["humidity"],
                 this.weatherData.icon = weather["currently"]["icon"]
                 console.log("Weather: ", this.weatherData)
+                this.setIcon();
             },
             err => console.error(err));
     }
@@ -75,5 +79,10 @@ export class WeatherComponent implements OnInit {
         } else {
             this.currentSpeedUnit = "kph";
         }
+    }
+
+    setIcon() {
+        this.icons.add("icon", this.weatherData.icon);
+        this.icons.play();
     }
 }
